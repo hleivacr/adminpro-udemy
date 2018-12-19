7c4524a2e05d0bae8191fa0c89ca602069e5ce26
“Hugo <hugoleivam@gmail.com>
2018-12-18 18:26:33 -0600

    martes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SettingsService } from './services/settings.service';
 
 // Rutas
 import { APP_ROUTES } from './app.routes';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 import { FormsModule } from '@angular/forms';
+import { from } from 'rxjs';
 
 
 
@@ -29,7 +31,7 @@ import { FormsModule } from '@angular/forms';
     PagesModule,
     FormsModule
   ],
-  providers: [],
+  providers: [SettingsService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
